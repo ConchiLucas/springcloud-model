5dc21bb12d2d8dbc2b3d2aeb0c6b3b4e91fe1d8e
ConchiLucas <137123860@qq.com>
2020-08-02 23:05:59 +0800

    first submit

---
+++ ccbc-server/src/main/java/com/ccbc/CcbcServerApplication.java
@@ -0,0 +1,19 @@
+package com.ccbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class CcbcServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcbcServerApplication.class, args);
+	}
+
+}
