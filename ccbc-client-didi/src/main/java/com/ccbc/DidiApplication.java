73901e5c4b532b67309d9f73d025c0b801bc473c
lukq <67592093>
2020-08-03 10:04:00 +0800

    增加client-didi

---
+++ ccbc-client-didi/src/main/java/com/ccbc/DidiApplication.java
@@ -0,0 +1,39 @@
+package com.ccbc;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class DidiApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(DidiApplication.class, args);
+	}
+
+
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		System.out.println("client:9002----running");
+		//传入SpringBoot应用的主程序
+		return application.sources(DidiApplication.class);
+	}
+
+	@Value("${server.port}")
+	String port;
+
+	@RequestMapping("/hi")
+	public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+		return "hi " + name + " ,i am from port:" + port;
+	}
+
+}
---
+++ ccbc-client-didi/src/test/java/com/ccbc/CcbcServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcbcServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
