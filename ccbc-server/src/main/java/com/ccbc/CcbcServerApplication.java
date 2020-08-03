0bd675f683afb8a5c1d67737b4968606142466fc
lukq <67592093>
2020-08-03 09:49:33 +0800

    增加commono

--- ccbc-server/src/main/java/com/ccbc/CcbcServerApplication.java
+++ ccbc-server/src/main/java/com/ccbc/CcbcServerApplication.java
@@ -2,11 +2,7 @@ package com.ccbc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @EnableEurekaServer
 @SpringBootApplication
@@ -15,7 +11,6 @@ public class CcbcServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CcbcServerApplication.class, args);
 	}
-
 
 
 
