8a05988c882d56afb936d5d81dacc48b97e3d4eb
lukq <67592093>
2020-08-03 10:12:49 +0800

    增加client-didi-api

---
+++ ccbc-client-didi-api/src/main/java/com/ccbc/didi/DidiService.java
@@ -0,0 +1,7 @@
+package com.ccbc.didi;
+
+public interface DidiService {
+
+    public void execute();
+
+}
---
+++ ccbc-client-didi/src/main/java/com/ccbc/didi/DidiServiceImpl.java
@@ -0,0 +1,13 @@
+package com.ccbc.didi;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DidiServiceImpl implements DidiService{
+
+
+    @Override
+    public void execute() {
+
+    }
+}
